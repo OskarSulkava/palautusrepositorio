9ec5edd8eb139a6f7ae5b3342a97879f2181ffb2
OskarSulkava <oskarsulkava@gmail.com>
2021-01-04 12:14:11 +0200

    2.11

--- Osa 2/puhelinluettelo/src/App.js
+++ Osa 2/puhelinluettelo/src/App.js
@@ -1,18 +1,25 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Contacts from './components/Contacts'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 
 const App = () => {
-    const [person, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1231244' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [person, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [newFilter, setNewFilter] = useState('')
+
+    const hook = () => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }
+    
+    useEffect(hook, [])
     
 
     const addPerson = (event) => {
